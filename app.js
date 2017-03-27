ce7fb4df3e6dd5d9db6c93f60810ce4e3ca8b516
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-27 20:47:24 +0300

    update

--- app.js
+++ app.js
@@ -2,23 +2,10 @@ var express = require('express');
 var path = require('path');
 var app = express();
 // var server = http.createServer(app);
-var io = require('socket.io').listen(app, options);
+// var io = require('socket.io').listen(app, options);
 // Define the port to run on
 var port = process.env.PORT || 3000;
-io.sockets.on('connection', function (client) {
-    //подписываемся на событие message от клиента
-    client.on('message', function (message) {
-        try {
-            //посылаем сообщение себе
-            client.emit('message', message);
-            //посылаем сообщение всем клиентам, кроме себя
-            client.broadcast.emit('message', message);
-        } catch (e) {
-            console.log(e);
-            client.disconnect();
-        }
-    });
-});
+
 app.use(express.static('client'));
 var router = express.Router();
 
