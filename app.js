b86e57cb175208e3cb82f3ab2a87194e92e43ce0
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-27 21:48:40 +0300

    change server

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-// var server = http.createServer(app);
-// var io = require('socket.io').listen(app, options);
-// Define the port to run on
+var https = require('https');
+var server = https.createServer(app);
 var port = process.env.PORT || 3000;
+var portSoket = process.env.PORT || 5000;
+var options = {
+//    'log level': 0
+};
+var io = require('socket.io').listen(server, options);
+io.sockets.on('connection', function (client) {
+    //подписываемся на событие message от клиента
+    client.on('message', function (message) {
+        try {
+            //посылаем сообщение себе
+            client.emit('message', message);
+            //посылаем сообщение всем клиентам, кроме себя
+            client.broadcast.emit('message', message);
+        } catch (e) {
+            console.log(e);
+            client.disconnect();
+        }
+    });
+});
 
 app.use(express.static('client'));
 var router = express.Router();
 
 // Listen for requests
+server.listen(portSoket);
 app.listen(port);
