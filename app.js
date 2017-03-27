09af2b8830d481957754c07e6262fdfa99a6d607
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-27 20:31:16 +0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
+var server = http.createServer(app);
+var io = require('socket.io').listen(server, options);
 // Define the port to run on
 var port = process.env.PORT || 3000;
-
+io.sockets.on('connection', function (client) {
+    //подписываемся на событие message от клиента
+    client.on('message', function (message) {
+        try {
+            //посылаем сообщение себе
+            client.emit('message', message);
+            //посылаем сообщение всем клиентам, кроме себя
+            client.broadcast.emit('message', message);
+        } catch (e) {
+            console.log(e);
+            client.disconnect();
+        }
+    });
+});
 app.use(express.static('client'));
 var router = express.Router();
 
--- client/assets/js/chat.js
+++ client/assets/js/chat.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var socket = io.connect('https://chat-and-game.herokuapp.com:5000');
+    var socket = io.connect('https://chat-and-game.herokuapp.com:3000');
     var name = 'Guest_' + (Math.round(Math.random() * 10000));
     var messages = $("#messages");
     var message_txt = $("#message_text")
