9b38656dca926d47c5afe3fcd1288869b3dce16b
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-27 20:32:53 +0300

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server, options);
+// var server = http.createServer(app);
+var io = require('socket.io').listen(app, options);
 // Define the port to run on
 var port = process.env.PORT || 3000;
 io.sockets.on('connection', function (client) {
