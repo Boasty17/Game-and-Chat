a2e9ef4c5c32de954c20011302a5b28f3fb12a10
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-29 17:39:43 +0300

    change server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/client'));
 app.get('/', function (req, res) {
